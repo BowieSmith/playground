86895919b46ff52f8d1c6bd4c33eced1f4645742
Bowie Smith <r.bowie.smith@gmail.com>
2019-03-02 18:05:24 -0500

    Mergesort and countsort in python

--- python/sorting.py
+++ python/sorting.py
@@ -1,7 +1,21 @@
 import math
 import random
 
-### Heapsort ###
+### Sort tester ###
+def test_sort(sort_function, number_trials, list_size, domain_size):
+    for n in range(0, number_trials):
+        l = [random.randint(1, domain_size) for _ in range(0, list_size)]
+        l_copy = l[:]
+        sort_function(l)
+        if sorted(l_copy) != l:
+            print("Original List:      ", l_copy)
+            print("Sorted correctly:   ", sorted(l_copy))
+            print("Sorted incorrectly: ", l)
+            return "Test Failed"
+    return "Test Passed!"
+
+
+### Heapsort ### Sorts list in-place. O(nlogn) worst-case.
 def max_heapify(lst, i, n):
 
     def value_at(i):
@@ -33,7 +47,7 @@ def heapsort(lst):
         max_heapify(lst, 1, size - 1)
 
 
-### Quicksort ###
+### Quicksort ### sorts list in-place. O(nlogn) average-case.
 def partition(lst, start, end):
     pivot_index = random.randint(start, start + (end - start))
     pivot_value = lst[pivot_index]
@@ -47,8 +61,62 @@ def partition(lst, start, end):
     lst[i + 1], lst[end] = lst[end], lst[i + 1]
     return i + 1
 
-def quicksort(lst, start, end):
-    if start < end:
-        pivot_index = partition(lst, start, end);
-        quicksort(lst, start, pivot_index - 1)
-        quicksort(lst, pivot_index + 1, end)
+def quicksort(lst):
+    def helper(list, start, end):
+        if start < end:
+            pivot_index = partition(lst, start, end);
+            helper(lst, start, pivot_index - 1)
+            helper(lst, pivot_index + 1, end)
+    helper(lst, 0, len(lst) - 1)
+
+
+### Mergesort ### sorts list using copies, O(n) space complexity. O(nlogn) worst-case. 
+def merge(L1, L2):
+    merged_list = []
+    while True:
+        if (L1[0] <= L2[0]):
+            merged_list.append(L1[0])
+            del L1[0]
+        else:
+            merged_list.append(L2[0])
+            del L2[0]
+        if not L1:
+            merged_list.extend(L2)
+            return merged_list
+        if not L2:
+            merged_list.extend(L1)
+            return merged_list
+
+def mergesort(lst):
+    if len(lst) == 1:
+        return lst
+    middle_index = math.floor(len(lst) / 2);
+    L1 = mergesort(lst[:middle_index])
+    L2 = mergesort(lst[middle_index:])
+    lst[:] = merge(L1, L2)
+    return lst
+
+
+### Countsort ### linear-Time sort. Good for small domain of integers. O(n) on well behaved lists.
+def build_frequency_list(l):
+    frq_list = [0 for _ in range(0, max(l) + 1)]
+    for i in l:
+        frq_list[i] += 1
+    return frq_list
+
+def running_total(l):
+    run_total = []
+    run_total.append(l[0])
+    for i in range(0, len(l) - 1):
+        run_total.append(run_total[i] + l[i + 1])
+    return run_total
+
+def countsort(lst):
+    sorted_list = [0 for _ in range(0, len(lst))]
+    freq_list = build_frequency_list(lst)
+    freq_total = running_total(freq_list)
+    for i in range(len(lst) - 1, -1, -1):
+        sorted_list[freq_total[lst[i]] - 1] = lst[i]
+        freq_total[lst[i]] -= 1
+    lst[:] = sorted_list
+    return sorted_list
